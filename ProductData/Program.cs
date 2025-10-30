57c8c2f024a74a3ddbc0cf569cd34681455eb5c5
siyanav <siyana.vasileva.2026@spgke.com>
2025-10-30 08:56:32 +0200

    .

--- ProductData/Program.cs
+++ ProductData/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class ProductData
+{
+    public static void Main(string[] args)
+    {
+        double productPrice = 45.99; 
+        int quantity = 250;
+
+        double vatRate = 0.20;
+
+        const double TOTAL_PROFIT_GOAL = 5000.00;
+
+        double totalReavenue = productPrice * quantity
+
+        Console.WriteLine($"Цена на продукт: {productPrice:C}");
+        Console.WriteLine($"Продадено количество: " + quantity);
+        Console.WriteLine($"Общ приход: {totalReavenue:C}");
+        Console.WriteLine($"Печалба: {TOTAL_PROFIT_GOAL:C}");
+    }
+}
