8ec8d90cc6ce0a77cfcd8b6ea4eca6ea525544d9
siyanav <siyana.vasileva.2026@spgke.com>
2025-11-06 08:41:23 +0200

    program2

---
+++ Program2/Program.cs
@@ -0,0 +1,23 @@
+﻿public class InventoryUpdate
+{
+    Public static void Main(string[] args)
+    {
+        int inventory = 500;
+        int itemsSold = 75;
+        int invoiceNumber = 2024001;
+        int lateDays = 0;
+
+        //Съкратен запис за изваждане (Продажба)
+        inventory -= itemsSold;
+        Console.WriteLine($"Оставаща наличност: " + inventory);
+
+        //Съкратен запис за умножение (Месечно натрупване)
+        lateDays = 1;
+        lateDays *= 3; //Натрупване на забава от 3 месеца
+        Console.WriteLine($"Натрупани дни забавяне: " + lateDays);
+
+        //Инкрементиране (Следваща фактура)
+        invoiceNumber++; 
+        Console.WriteLine(&"Натрупани дни забавяне: " + lateDays);
+    }
+}
